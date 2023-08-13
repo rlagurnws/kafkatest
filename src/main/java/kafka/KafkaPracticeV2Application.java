7674ed272b1fb13b1e8b3954065cc54e6770a729
DESKTOP-8DJ6DD6\user <gjh5708@naver.com>
2023-08-13 17:21:34 +0900

    230813-ReplyingKafkaTemplate

---
+++ src/main/java/kafka/KafkaPracticeV2Application.java
@@ -0,0 +1,13 @@
+package kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPracticeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPracticeV2Application.class, args);
+	}
+
+}
